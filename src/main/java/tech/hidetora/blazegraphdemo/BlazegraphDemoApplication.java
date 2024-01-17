2e6fa63788d557d64b7e203ae90321b5d91cbaaa
Hidetora <idetoratojo1@gmail.com>
2024-01-17 16:39:18 +0100

    Init

---
+++ src/main/java/tech/hidetora/blazegraphdemo/BlazegraphDemoApplication.java
@@ -0,0 +1,13 @@
+package tech.hidetora.blazegraphdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlazegraphDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlazegraphDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/hidetora/blazegraphdemo/BlazegraphDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.hidetora.blazegraphdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlazegraphDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
